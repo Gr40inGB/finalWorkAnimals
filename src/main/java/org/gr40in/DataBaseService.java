18f97d4d8fbc7ec54cf118612beef9fa0d1eb2d1
Rishat_Imangulov <gr40in@yandex.ru>
2023-11-05 01:08:07 +0500

    test + ubuntu

--- src/main/java/org/gr40in/DataBaseService.java
+++ src/main/java/org/gr40in/DataBaseService.java
@@ -2,6 +2,7 @@ package org.gr40in;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
@@ -14,21 +15,24 @@ public class DataBaseService {
     public static Connection getConnection() {
 
         Path DBpath = Path.of("src/main/resources/database.properties");
-
+        Connection resultConnection = null;
         Properties dbProperties = new Properties();
-        try {
-            dbProperties.load(Files.newInputStream(DBpath));
+
+        try (InputStream is = Files.newInputStream(DBpath)) {
+            dbProperties.load(is);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Properties read ERROR: " + e.getMessage());
         }
 
         try {
-            return DriverManager.getConnection(dbProperties.getProperty("DB_URL"),
+            resultConnection = DriverManager.getConnection(dbProperties.getProperty("DB_URL"),
                     dbProperties.getProperty("DB_USER"),
                     dbProperties.getProperty("DB_PASSWORD"));
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("Connection ERROR: " + e.getMessage());
         }
+
+        return resultConnection;
     }
 
 }
--- src/main/java/org/gr40in/Main.java
+++ src/main/java/org/gr40in/Main.java
@@ -1,7 +1,26 @@
 package org.gr40in;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        DataBaseService.getConnection();
+        List<String> stringList = new ArrayList<>();
+
+        String query = "SELECT * FROM auto";
+        Connection connection = DataBaseService.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("REGNUM"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
